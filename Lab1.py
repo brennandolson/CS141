435012dcb26c6189a83bdb49a4fd45d3f848b720
Brennan <bpdolson@email.wm.edu>
2018-05-30 09:31:05 -0400

    finalized lab 1

--- Lab1.py
+++ Lab1.py
@@ -1,2 +1,52 @@
-#Lab 1
-#CS141 - Summer 2018
+# CS141 - Summer 2018
+# Lab 1
+
+# Write code to answer the following questions.
+# The first question is filled in for you to show how you
+#   should format your output.
+
+# Question 0 (simple types):
+# Consider the follwing code:
+x = 1000
+y = 200.4
+z = "300"
+# What types are x, y, and z?
+print ("Question 0:")
+print ("x is type", type(x))
+print ("y is type", type(y))
+print ("z is type", type(z))
+print () #empty print outputs a newline
+
+# Question 1 (addition):
+# What is x + x?
+# What is z + z?
+# What happens if you try to run x + z?
+
+
+
+# Question 2 (multiplication):
+# What is x * 3?
+# What is y * 0.4?
+# What is z * 4?
+# What happens if you try to run z * 1.5?
+# What type is x * 3?
+# What type is x * 3.0?
+
+
+
+# Question 3 (other operations):
+# What is 21 / 5?
+# What is 2 ** 5?
+# What, in words, does ** do?
+
+
+
+# Question 4 (cost of a car):
+# A certain car costs $20,000.
+# The tax rate on the car is 8%.
+# Store these two pieces of information as two 
+#   sensibly named variables and calculate the
+#   final cost of the car using the formula 
+#   final_cost = (1 + tax_rate) * base_cost
+
+
