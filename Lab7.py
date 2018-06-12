e48983e7365ffd3538cee8db2802f84bde87e8e6
Brennan <bpdolson@email.wm.edu>
2018-06-12 16:48:53 -0400

    Added lab 7

---
+++ Lab7.py
@@ -0,0 +1,30 @@
+# CS141 - Summer 2018
+# Lab 7 - functions
+
+# Create the following functions:
+
+# isPrime()
+# input: an integer n >= 1
+# output: True if n is prime, otherwise False
+# Ex: irPrime(2) returns True
+#     isPrime(91) returns False
+
+
+
+# isPalindrome()
+# input: a string s
+# output: True if n is a palindrome, otherwise False
+# Ex: isPalindrome("abba") returns True
+#     isPalindrome("abab") returns False
+
+
+
+# containsPalindrome()
+# input: a string s
+#        an integer k >= 1
+# output: True if s contains a palindrome of length k
+#         False otherwise
+# Ex: containsPalindrome("cabbage", 4) returns True 
+#     containsPalindrome("cabbage", 3) returns False
+
+
